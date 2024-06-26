0cf864f0336b3818b8e20ba081f3139de7d55829
Julius Drago <yuriy.paramonov@outlook.com>
2024-06-26 18:19:35 -0400

    My current config updated i3

---
+++ Init.sh
@@ -0,0 +1,3 @@
+# installing Fisher
+
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
--- Update.sh
+++ Update.sh
@@ -1,5 +1,11 @@
-echo "Removing nvim config"
-rm -r -f ~/.config/nvim/custom
-echo "Coping our nvim config"
-cp -r ./MyNvChadConfig ~/.config/nvim/lua/custom
+echo "Removing kitty and tmux config"
+
+rm -rf ~/.config/kitty
+rm -rf ~/.config/tmux
+rm -rf ~/.config/i3
+rm -rf ~/.config/fish
+
+cp -r ./config/* ~/.config
+# rm -r -f ~/.config/nvim/custom echo "Coping our nvim config"
+# cp -r ./MyNvChadConfig ~/.config/nvim/lua/custom
 echo "Done"
