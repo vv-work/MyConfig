f1ad795c2b5ebfda1eba9b53534d38a9721205ec
Julius Drago <yuriy.paramonov@outlook.com>
2024-07-04 02:00:02 -0400

    updated

--- Update.sh
+++ Update.sh
@@ -1,12 +1,40 @@
-echo "Removing kitty and tmux config"
+echo "Removing kitty and tmux config" 
+# Define colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
 
-rm -rf ~/.config/kitty
-rm -rf ~/.config/tmux
+# Example usage
+# echo -e "${GREEN}This is green text${NC}"
+# echo -e "${YELLOW}This is yellow text${NC}"
+# echo -e "${BLUE}This is blue text${NC}"
+
+
+my_array=("kitty" "tmux")
+
+# Looping through an array
+for element in "${my_array[@]}"; do
+  rm -rf ~/.config/$element 
+  echo -e "${RED}Removed ${element} ${NC}"
+  # rm -rf ~/.config/tmux
+done
+
+echo -e "${NC}"
+
+for element in "${my_array[@]}"; do
+  cp -r ./config/$element ~/.config/
+  echo "Copied $element"
+  # rm -rf ~/.config/tmux
+done
 # I can't remove i3 because it's configuration is not finished 
 # rm -rf ~/.config/i3
-rm -rf ~/.config/fish
+# rm -rf ~/.config/fish
 
-cp -r ./config/* ~/.config
+# cp -r ./config/* ~/.config
+# rm -rf ~/.config/kitty 
+# rm -rf ~/.config/tmux
 # rm -r -f ~/.config/nvim/custom echo "Coping our nvim config"
 # cp -r ./MyNvChadConfig ~/.config/nvim/lua/custom
 echo "Done"
