2993f52afee5ea07c755510ddac864520954dc0c
vv-work <yuriy.paramonov@outlook.com>
2024-10-03 18:51:09 -0400

    🐙🐱   Backup Neovim settings after update

---
+++ scripts/backup_nvim.sh
@@ -0,0 +1,12 @@
+# Global variables for the backup_nvim.sh script
+my_config_dir = "../config/"
+linunx_config_dir = "~/.config/"
+
+nvim_sys_dir = linunx_config_dir + "nvim/"
+nvim_my_dir = my_config_dir + "nvim/"
+
+prefix_git = "🐙🐱"
+prefix_vim_backup = " "
+sufix_done = ""
+
+
