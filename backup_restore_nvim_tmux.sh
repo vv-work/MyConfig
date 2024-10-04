68d76e413f3fd09dbf39b73737ff39c2a565908b
Julius P <julius.paramono@gmail.com>
2024-10-03 18:59:49 -0700

    successfully resotred Github files

--- backup_restore_nvim_tmux.sh
+++ backup_restore_nvim_tmux.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Global Variables
-root_dir="/home/shared/MyConfig/"
+root_dir="$(pwd)/"
 my_config_dir="${root_dir}config/"
 linux_config_dir="$HOME/.config/"
 
 nvim_sys_dir="${linux_config_dir}nvim/"
-nvim_my_dir="${my_config_dir}config/nvim/"
+nvim_my_dir="${my_config_dir}nvim/"
 tmux_sys_file="$HOME/.tmux.conf"
 tmux_my_file="${my_config_dir}tmux.conf"
 
@@ -20,8 +20,8 @@ commit_and_push() {
     local dir="$1"
     local message="$2"
     
-    cd root_dir
-    git add "."
+    cd "$root_dir" || exit
+    git add "$dir"
     git commit -m "$message"
     git push origin main
 }
@@ -53,7 +53,6 @@ backup_tmux() {
     # Check if tmux.conf exists and push existing changes
     if [ -f "$tmux_my_file" ]; then
         commit_and_push "$tmux_my_file" "$prefix_git $prefix_tmux_backup Backup existing TMUX settings"
-        rm -f "$tmux_my_file"
         echo "$prefix_git $prefix_tmux_backup Backup and push of existing TMUX settings completed."
     fi
     
@@ -67,11 +66,46 @@ backup_tmux() {
     echo "$prefix_git - $suffix_done TMUX backup completed successfully."
 }
 
-# Main function to trigger backups
-main() {
-    backup_nvim
-    backup_tmux
+# Function to restore Neovim settings
+restore_nvim() {
+    echo "$prefix_vim_backup Restoring Neovim settings..."
+    rm -rf ~/.config/nvim
+    rm -rf ~/.local/state/nvim
+    rm -rf ~/.local/share/nvim   
+    # Copy the Neovim configuration from myConfig to the system
+    rsync -av --delete "$nvim_my_dir" "$nvim_sys_dir"
+    
+    # Completion message
+    echo "$prefix_git - $suffix_done Neovim restore completed successfully."
 }
 
-# Execute the main function
-main
+# Function to restore TMUX settings
+restore_tmux() {
+    echo "$prefix_tmux_backup Restoring TMUX settings..."
+    
+    # Copy the tmux.conf from myConfig to the system
+    cp "$tmux_my_file" "$tmux_sys_file"
+    
+    # Completion message
+    echo "$prefix_git - $suffix_done TMUX restore completed successfully."
+}
+
+# Main function to trigger backups or restores
+main() {
+    case "$1" in
+        backup)
+            backup_nvim
+            backup_tmux
+            ;;
+        restore)
+            restore_nvim
+            restore_tmux
+            ;;
+        *)
+            echo "Usage: $0 {backup|restore}"
+            ;;
+    esac
+}
+
+# Execute the main function with the argument passed
+main "$1"
---
+++ docs/00🔝_Top_CLI_tools.md
@@ -0,0 +1,15 @@
+# Top CLI Tools
+
+## New 
+
+- `rsync`
+- `bat` view file content in terminal
+
+## Medium New
+
+- htop
+- touch
+
+## Long Term
+
+- cd
